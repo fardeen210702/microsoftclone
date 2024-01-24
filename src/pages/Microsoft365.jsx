0b8428b485144cf666bad5c057a4e3342561d13e
Fardeen <fardeenmohd1251@gmail.com>
2024-01-24 16:09:11 +0530

    added x box comp

--- src/pages/Microsoft365.jsx
+++ src/pages/Microsoft365.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import Micro365landing from '../components/Micro365landing'
 import img from '../images/imglanding.avif'
 import MicroLanding365two from '../components/MicroLanding365two'
-import Microlandingthree from '../components/microlandingthree'
-
+import Microlandingthree from '../components/Microlandingthree'
 function Microsoft365() {
   return (<div className='relative'>
     <div className='slider w-full  flex justify-center items-end relative h-[200px] md:h-[500px] sm:h-[300px] lg:h-[600px]'>
