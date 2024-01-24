b91df6ce6923d1fd01b583706f15c3b853a0cc1e
Fardeen <fardeenmohd1251@gmail.com>
2024-01-24 15:55:35 +0530

    added responsivenesss

--- src/components/Landingpageinnercont.jsx
+++ src/components/Landingpageinnercont.jsx
@@ -11,8 +11,8 @@ function Landingpageinnercont({item}) {
             <img src={IMG} className='hidden lg:flex w-full' alt="" />
         </div>
         <div className="details flex flex-col gap-6 p-4 items-start lg:absolute lg:top-[20%] lg:right-20 lg:w-1/3">
-            <h1 className='text-3xl font-bold lg:text-5xl'>{title}</h1>
-            <p className='lg:text-2xl'>{p}</p>
+            <h1 className='text-3xl font-bold lg:text-4xl'>{title}</h1>
+            <p className='lg:text-xl'>{p}</p>
             <button className='flex items-center gap-2 bg-blue-500 text-white p-2 rounded-sm'>{btn} <FaChevronRight/></button>
         </div>
 
